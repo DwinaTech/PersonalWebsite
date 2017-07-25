3a45b10f4661ddc31f428d4b715c9430a9d09fd7
MrDwina <diwina1993@hotmail.com>
2017-07-25 22:48:36 +0100

    edit about me function

--- js/functional.js
+++ js/functional.js
@@ -1,11 +1,11 @@
 /*==============================================//
 // ======== Hide Textarea palceholder ========== //
 //=============================================*/
-var myTextArea = document.querySelectorAll("textarea");
+window.addEventListener('load', function(){
+ 'use strict'; var myTextArea = document.querySelectorAll("textarea");
 var i;
 for (i in myTextArea) {
 	myTextArea[i].onfocus = function () {
-		'use strict';
 		// store Textarea placeholder Attribute in data-place Attribute
 		this.setAttribute('data-place', this.getAttribute('placeholder'));
 		// Empty Placeholder Attribute
@@ -13,21 +13,22 @@ for (i in myTextArea) {
 	};
 
 	myTextArea[i].onblur = function () {
-		'use strict';
 		// Get placeholder Attribute from data-place Attribute
 		this.setAttribute('placeholder', this.getAttribute('data-place'));
 		// Empty data-place Attribute
 		this.setAttribute('data-place', '');
 	};
 }
+});
 /*==============================================//
 // ========== Hide Input palceholder =========== //
 //=============================================*/
+window.addEventListener('load', function(){
+	'use strict';
 var myInput = document.querySelectorAll("input");
 var i;
 for (i in myInput) {
 	myInput[i].onfocus = function () {
-		'use strict';
 		// store placeholder Attribute in Backup Attribute
 		this.setAttribute('data-place', this.getAttribute('placeholder'));
 		// Empty Placeholder
@@ -35,21 +36,22 @@ for (i in myInput) {
 	};
 
 	myInput[i].onblur = function () {
-		'use strict';
 		// Get placeholder Attribute from data-place Attribute
 		this.setAttribute('placeholder', this.getAttribute('data-place'));
 		// Empty data-place
 		this.setAttribute('data-place', '');
 	};
 }
+});
 /*==============================================//
 // ========== Start About me ================= //
 //=============================================*/
-const moreInfo = document.getElementById('aboutInfor');
-const infoPragraph = document.getElementsByClassName('aboutMoreInfor');
-//const about = document.querySelector('.about');
-moreInfo.addEventListener('click', function () {
+window.addEventListener('load', function(){
+	const moreInfo = document.getElementById('aboutInfor');
+	const infoPragraph = document.getElementsByClassName('aboutMoreInfor');
+	moreInfo.addEventListener('click', function () {
 	infoPragraph[0].classList.toggle('aboutInfoShow');
+});
 });
 /*==============================================//
 // ========== Start Form Validation ========== //
@@ -67,11 +69,11 @@ firstName.addEventListener('keyup', function (params) {
 	} else {
 		errorBox.innerHTML = "&check;";
 		errorBox.style.color = "green";
-		firstName.style.borderColor = "green"
+		firstName.style.borderColor = "green";
 		errorBox.style.display = "block";
 		errorBox.style.margin = "-10px 0px -10px 0";
 	}
-})
+});
 
 const lastName = document.forms[0].lastName;
 lastName.addEventListener('keyup', function (params) {
@@ -85,11 +87,11 @@ lastName.addEventListener('keyup', function (params) {
 	} else {
 		errorBox.innerHTML = "&check;";
 		errorBox.style.color = "green";
-		lastName.style.borderColor = "green"
+		lastName.style.borderColor = "green";
 		errorBox.style.display = "block";
 		errorBox.style.margin = "-10px 0px -10px 0";
 	}
-})
+});
 
 const email = document.forms[0].email;
 email.addEventListener('keyup', function (params) {
@@ -103,11 +105,11 @@ email.addEventListener('keyup', function (params) {
 	} else {
 		errorBox.innerHTML = "&check;";
 		errorBox.style.color = "green";
-		email.style.borderColor = "green"
+		email.style.borderColor = "green";
 		errorBox.style.display = "block";
 		errorBox.style.margin = "-10px 0px -10px 0";
 	}
-})
+});
 const confirmEmail = document.forms[0].confirmEmail;
 confirmEmail.addEventListener('keyup', function (params) {
 	const errorBox = params.target.nextElementSibling;
@@ -119,11 +121,11 @@ confirmEmail.addEventListener('keyup', function (params) {
 	} else {
 		errorBox.innerHTML = "&check;";
 		errorBox.style.color = "green";
-		confirmEmail.style.borderColor = "green"
+		confirmEmail.style.borderColor = "green";
 		errorBox.style.display = "block";
 		errorBox.style.margin = "-10px 0px -10px 0";
 	}
-})
+});
 
 const password = document.forms[0].password;
 password.addEventListener('keyup', function (params) {
@@ -138,11 +140,11 @@ password.addEventListener('keyup', function (params) {
 	} else {
 		errorBox.innerHTML = "&check;";
 		errorBox.style.color = "green";
-		password.style.borderColor = "green"
+		password.style.borderColor = "green";
 		errorBox.style.display = "block";
 		errorBox.style.margin = "-10px 0px -10px 0";
 	}
-})
+});
 const confirmPassword = document.forms[0].confirmPassword;
 confirmPassword.addEventListener('keyup', function (params) {
 	const errorBox = params.target.nextElementSibling;
@@ -157,10 +159,10 @@ confirmPassword.addEventListener('keyup', function (params) {
 		errorBox.style.color = "green";
 		errorBox.style.display = "block";
 		errorBox.style.margin = "-10px 0px 10px 0";
-		confirmPassword.style.borderColor = "green"
+		confirmPassword.style.borderColor = "green";
 	}
 	
-})
+});
 
 const formSubmit = document.getElementById('formSubmit');
 formSubmit.addEventListener('click', function (event) {
